6cdbf1b3909deb9ba96f65a6f543846f46eb08f5
d2verb <licprog@gmail.com>
2019-06-23 03:08:39 +0900

    Can execute program from file

--- main.go
+++ main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/user"
 
+	"github.com/d2verb/monkey/evaluator"
+	"github.com/d2verb/monkey/lexer"
+	"github.com/d2verb/monkey/object"
+	"github.com/d2verb/monkey/parser"
 	"github.com/d2verb/monkey/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	if len(os.Args) < 2 {
+		repl.Start(os.Stdin, os.Stdout)
+	} else {
+		content, err := ioutil.ReadFile(os.Args[1])
+		if err != nil {
+			fmt.Println("Error: ", err)
+			os.Exit(1)
+		}
+
+		l := lexer.New(string(content))
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			for _, msg := range p.Errors() {
+				fmt.Println(msg)
+			}
+			os.Exit(1)
+		}
+
+		env := object.NewEnvironment()
+		evaluated := evaluator.Eval(program, env)
+
+		if evaluated != nil {
+			fmt.Println(evaluated.Inspect())
+		}
+
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language\n",
-		user.Username)
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
 }
