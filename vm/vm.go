ae7ff0f37d392c42dda65af880f7744021798791
d2verb <licprog73@gmail.com>
2024-05-12 02:54:10 +0900

    Fisrt VM code

---
+++ vm/vm.go
@@ -0,0 +1,64 @@
+package vm
+
+import (
+	"fmt"
+
+	"github.com/d2verb/mankey/code"
+	"github.com/d2verb/mankey/compiler"
+	"github.com/d2verb/mankey/object"
+)
+
+const StackSize = 2048
+
+type VM struct {
+	constants    []object.Object
+	instructions code.Instructions
+
+	stack []object.Object
+	sp    int
+}
+
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		constants:    bytecode.Constants,
+		instructions: bytecode.Instructions,
+
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+	}
+}
+
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.instructions); ip++ {
+		op := code.Opcode(vm.instructions[ip])
+
+		switch op {
+		case code.OpConstant:
+			constantIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
+			err := vm.push(vm.constants[constantIndex])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
+	return nil
+}
